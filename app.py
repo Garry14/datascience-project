a3d4042c2c174bcdb550f80f12e1971267924891
garrydev <garryandi@badr-interactive.com>
2026-04-05 14:19:45 +0700

    Adding EDA

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import streamlit as st
 
-st.set_page_config(page_title="Churn Prediction App", layout="wide")
+st.set_page_config(page_title="Telco Churn Prediction", layout="wide")
 
-st.markdown("""
+st.markdown(
+    """
+    <h1 style='text-align: center; color: #4CAF50;'>
+        📊 Telco Customer Churn Prediction
+    </h1>
+    <h4 style='text-align: center;'>
+        Analisis & Prediksi Customer yang Berpotensi Churn
+    </h4>
+    <hr>
+""",
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    """
 Gunakan menu di sidebar untuk navigasi:
 - Introduction
 - EDA
 - Modeling
 - Prediction
-""")
+"""
+)
--- pages/2_EDA.py
+++ pages/2_EDA.py
@@ -12,14 +12,25 @@ data = pd.read_excel("./Telco_customer_churn.xlsx")
 st.subheader("📂 Dataset Overview")
 st.dataframe(data.head())
 
-st.subheader("📈 Distribusi Target Variable (Churn)")
-st.bar_chart(data["Churn Value"].value_counts())
+# st.subheader("📈 Distribusi Target Variable (Churn)")
+# st.bar_chart(data["Churn Value"].value_counts())
 
-st.subheader("📉 Distribusi Fitur Numerik")
+# st.subheader("📉 Distribusi Fitur Numerik")
+# st.dataframe(data.describe())
+
+# st.subheader("📊 Distribusi Fitur Kategorikal")
+# categorical_columns = data.select_dtypes(include=["object"]).columns
+# for col in categorical_columns:
+#     st.write(f"### {col}")
+#     st.bar_chart(data[col].value_counts())
+st.subheader("🔍 Data Quality Check")
+st.write(f"Shape of the dataset: {data.shape}")
+
+st.write("Info:")
+st.text(data.info())
+
+st.write("Description:")
 st.dataframe(data.describe())
 
-st.subheader("📊 Distribusi Fitur Kategorikal")
-categorical_columns = data.select_dtypes(include=["object"]).columns
-for col in categorical_columns:
-    st.write(f"### {col}")
-    st.bar_chart(data[col].value_counts())
+st.write("Missing Values:")
+st.dataframe(data.isnull().sum())   
